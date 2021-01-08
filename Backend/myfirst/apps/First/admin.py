2b4e22827804f3ee64b21f01318bfe4b5e5c08cf
FmFromB <63696790+FmFromB@users.noreply.github.com>
2021-01-09 02:35:21 +1000

    Delete admin.py

--- Backend/myfirst/apps/First/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Client, Realtor, Requirement, Offer, Deal
-
-admin.site.register(Client)
-admin.site.register(Realtor)
-admin.site.register(Requirement)
-admin.site.register(Offer)
-admin.site.register(Deal)
